c10970393bf4ed46152f9e9c0d6598e2e292f56b
fhernand <fhernand@users.noreply.github.com>
2017-09-16 19:12:32 +0200

    Update unicornhat_test.js

--- unicornhat_test.js
+++ unicornhat_test.js
@@ -1,6 +1,6 @@
-var ws281x = require('node-rpi-ws281x-native');
+var ws281x = require('rpi-ws281x-native');
 
-var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
+var NUM_LEDS = 32,
     pixelData = new Uint32Array(NUM_LEDS);
 
 ws281x.init(NUM_LEDS);
