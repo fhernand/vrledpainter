93f16dffe8f4d36d2d9e3f37de1988a145ea7d30
fhernand <fhernand@users.noreply.github.com>
2017-09-16 19:05:52 +0200

    Update unicornhat_test.js

--- unicornhat_test.js
+++ unicornhat_test.js
@@ -1,110 +1,39 @@
-var SPI = require('pi-spi');
+var ws281x = require('../lib/ws281x-native');
 
-function UnicornHatHD(device) {
-    this.matrix = [];
+var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
+    pixelData = new Uint32Array(NUM_LEDS);
 
-    for (var y=0; y<4; y++) {
-        this.matrix.push([]);
-        for (var x=0; x<4; x++) {
-            this.matrix[y].push({
-                r: 0,
-                g: 0,
-                b: 0
-            })
-        }
-    }
+ws281x.init(NUM_LEDS);
 
-    this.brightness = 0.5;
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(function () { process.exit(0); });
+});
 
-    this.spi = SPI.initialize(device);
+
+// ---- animation-loop
+var offset = 0;
+setInterval(function () {
+  for (var i = 0; i < NUM_LEDS; i++) {
+    pixelData[i] = colorwheel((offset + i) % 256);
+  }
+
+  offset = (offset + 1) % 256;
+  ws281x.render(pixelData);
+}, 1000 / 30);
+
+console.log('Press <ctrl>+C to exit.');
+
+
+// rainbow-colors, taken from http://goo.gl/Cs3H0v
+function colorwheel(pos) {
+  pos = 255 - pos;
+  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
+  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
+  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
 }
 
-UnicornHatHD.prototype.setBrightness = function (brightness) {
-    this.brightness = brightness;
-};
-
-UnicornHatHD.prototype.getBrightness = function () {
-    return this.brightness;
-};
-
-UnicornHatHD.prototype.setAll = function (r, g, b) {
-    if (r !== parseInt(r, 10)) {
-        throw 'r must be an integer';
-    }
-
-    if (g !== parseInt(g, 10)) {
-        throw 'r must be an integer';
-    }
-
-    if (b !== parseInt(b, 10)) {
-        throw 'b must be an integer';
-    }
-
-    for (var x=0; x<4; x++) {
-        for (var y=0; y<4; y++) {
-            this.matrix[x][y].r = r;
-            this.matrix[x][y].g = g;
-            this.matrix[x][y].b = b;
-        }
-    }
-};
-
-UnicornHatHD.prototype.setPixel = function (x, y, r, g, b) {
-    this.matrix[x][y].r = r;
-    this.matrix[x][y].g = g;
-    this.matrix[x][y].b = b;
-};
-
-UnicornHatHD.prototype.getPixel = function (x, y) {
-    return [
-        this.matrix[x][y].r,
-        this.matrix[x][y].g,
-        this.matrix[x][y].b
-    ];
-};
-
-UnicornHatHD.prototype.clear = function () {
-    this.setAll(0, 0, 0);
-};
-
-UnicornHatHD.prototype.show = function (flip_horizontal, flip_vertical) {
-    if (flip_horizontal) {
-        for(var i=0;i<this.matrix.length;i++){
-            this.matrix[i].reverse();
-        }
-    }
-
-    if (flip_vertical) {
-        this.matrix.reverse();
-    }
-
-    var buffer = new Buffer(16 * 16 * 3);
-
-    for (var y=0; y<4; y++) {
-        for (var x=0; x<4; x++) {
-            buffer[y * 16 * 3 + x * 3 + 0] = this.matrix[x][y].r * this.brightness;
-            buffer[y * 16 * 3 + x * 3 + 1] = this.matrix[x][y].g * this.brightness;
-            buffer[y * 16 * 3 + x * 3 + 2] = this.matrix[x][y].b * this.brightness;
-        }
-    }
-    
-    console.log("about to write");
-    this.spi.write(Buffer.concat([new Buffer([0x72]), buffer]), function (err) {
-        if (err) {
-            throw 'Something went wrong!';
-        }
-    });
-};
-
-var unicornHatHD = new UnicornHatHD('/dev/spidev0.0');
-
-unicornHatHD.setBrightness(1.0);
-
-unicornHatHD.setPixel(0, 0, 255, 0, 0);
-unicornHatHD.setPixel(1, 0, 0, 255, 0);
-unicornHatHD.setPixel(2, 0, 0, 0, 255);
-
-var flipHorizontal = true;
-var flipVertical = false;
-
-unicornHatHD.show(flipHorizontal, flipVertical);
+function rgb2Int(r, g, b) {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
