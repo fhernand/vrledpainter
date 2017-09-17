3201b16783e05b4a9bb836e2f664b6bc5b393db1
fhernand <fhernand@users.noreply.github.com>
2017-09-17 15:22:43 +0200

    Integrate LED control

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var io = require('socket.io-client');
 this.strokeBuffer = [];
 this.lastBufferProcess = 0;
 
+this.strokeActive = False;
+var self = this;
+
 if(io){
 	this.socket = io.connect('http://192.168.0.220:3002');
-	var self = this;
 	self.joinedRoom = 'theRoom';
 	this.socket.on('connect', function (socket) {
 		console.log('Connected!');
@@ -33,10 +35,12 @@ if(io){
 		if(event.stroke.owner === self.socket.owner) return;
 		//this.strokeBuffer.push(event);
 		console.log("New stroke");
+		self.strokeActive = True;
 	  });
 
 	  this.socket.on('endStroke', () => {
 		console.log("End stroke");
+		self.strokeActive = False;
 	  });
 
 	  this.socket.on('newPoints', event => {
@@ -56,3 +60,42 @@ if(io){
 	  });	
 	  	  
 }
+
+var ws281x = require('rpi-ws281x-native');
+
+var NUM_LEDS = 32,
+    pixelData = new Uint32Array(NUM_LEDS);
+
+ws281x.init(NUM_LEDS);
+
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(function () { process.exit(0); });
+});
+
+// ---- animation-loop
+var offset = 0;
+setInterval(function () {
+  if (self.strokeActive){
+  for (var i = 0; i < NUM_LEDS; i++) {
+		pixelData[i] = colorwheel((offset + i) % 256);
+	  }
+
+	  offset = (offset + 1) % 256;
+	  ws281x.render(pixelData);
+  }
+}, 1000 / 30);
+
+// rainbow-colors, taken from http://goo.gl/Cs3H0v
+function colorwheel(pos) {
+  pos = 255 - pos;
+  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
+  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
+  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
+}
+
+function rgb2Int(r, g, b) {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
+
