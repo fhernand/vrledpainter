40eef007338b3473ac65f043a504729cbe804c8e
fhernand <fhernand@users.noreply.github.com>
2017-09-24 17:04:09 +0200

    Brush sizes

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ this.LEDallblack = true;
 this.activeRed = 0;
 this.activeGreen = 0;
 this.activeBlue = 0;
+this.blackColor =  rgb2Int(0,0,0);
+
+this.sizeZero     = ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeOne      = ['0','0','0','0','0','0','0','0','0','0','0','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeTwo      = ['0','0','0','0','0','0','0','0','0','0','0','2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeThree    = ['0','0','0','0','0','0','0','0','0','0','0','3','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeFour     = ['0','0','0','0','0','0','0','0','0','0','0','3','3','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeFive     = ['0','0','0','0','0','0','0','0','0','0','1','3','3','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeSix      = ['0','0','0','1','1','0','0','0','0','0','2','3','3','2','0','0','0','0','0','1','1','0','0','0','0','0','0','0','0','0','0','0'];  
+this.sizeSeven    = ['0','0','1','1','1','1','0','0','0','1','3','3','3','3','1','0','0','0','1','2','2','1','0','0','0','0','0','1','1','0','0','0'];  
+this.sizeEight    = ['0','0','1','2','2','1','0','0','0','2','3','3','3','3','2','0','0','0','2','3','3','2','0','0','0','0','1','2','2','1','0','0'];  
+this.sizeNine     = ['0','0','2','3','3','2','0','0','1','3','3','3','3','3','3','1','0','1','3','3','3','3','1','0','0','0','2','3','3','2','0','0'];  
+this.sizeTen      = ['0','1','3','3','3','3','1','0','2','3','3','3','3','3','3','2','0','2','3','3','3','3','2','0','0','1','2','3','3','2','1','0'];  
+this.sizeEleven   = ['1','3','3','3','3','3','3','1','3','3','3','3','3','3','3','3','1','3','3','3','3','3','3','1','0','2','3','3','3','3','2','0'];  
+this.sizeTwelve   = ['2','3','3','3','3','3','3','2','3','3','3','3','3','3','3','3','2','3','3','3','3','3','3','2','1','2','3','3','3','3','2','1'];  
+this.sizeThirteen = ['3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','2','3','3','3','3','3','3','2','2','2','3','3','3','3','2','2'];  
+this.sizeFourteen = ['3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','2','3','3','3','3','3','3','2'];  
+this.sizeFifteen  = ['3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3','3'];  
+
+this.brushSizes = [sizeZero,sizeOne, sizeTwo, sizeThree,sizeFour, sizeFive, sizeSix, sizeSeven, sizeEight, sizeEleven, sizeTwelve, sizeThirteen, sizeFourteen, sizeFifteen];
+
+this.activeSize = brushSizes[0];
+
 var self = this;
 
 if(io){
@@ -44,6 +67,12 @@ if(io){
 		if(!event[0] || event[0].stroke.owner === self.socket.owner) return;
 		//console.log("New points");
 	  });
+	
+	  this.socket.on('newSize', event => {
+		if(!event[0] || event[0].stroke.owner === self.socket.owner) return;
+		console.log("Size changed");
+		self.activeSize = self.brushSizes[event.stroke.size];	 
+	  });	
 
 }
 
@@ -66,9 +95,28 @@ process.on('SIGINT', function () {
 // ---- animation-loop
 setInterval(function () {
 	if (self.strokeActive){
+		var lowModifier = 4;
+		var mediumModifier = 2;
 		var currentColor = rgb2Int(self.activeRed,self.activeGreen,self.activeBlue);
+		var currentColorLow = rgb2Int(Math.round(self.activeRed / lowModifier), Math.round(self.activeGreen / lowModifier), Math.round(self.activeBlue / lowModifier)); 
+		var currentColorMedium = rgb2Int(Math.round(self.activeRed / mediumModifier), Math.round(self.activeGreen / mediumModifier), Math.round(self.activeBlue / mediumModifier)); 
 		for (var i = 0; i < NUM_LEDS; i++) {
-			pixelData[i] = currentColor;//colorwheel((offset + i) % 256);
+			switch (activeSize[i]){
+				case 0:
+					pixelData[i] = self.blackColor;
+					break;
+				case 1:
+					pixelData[i] = currentColorLow;
+					break;					
+				case 2:
+					pixelData[i] = currentColorMedium;
+					break;					
+				case 3:
+					pixelData[i] = currentColor;
+					break;
+				default:		
+					pixelData[i] = colorwheel((offset + i) % 256);
+			}
 		}
 		ws281x.render(pixelData);
 		if(self.LEDallblack){
