eb3329e184cadaa64fd6c08e4b9dce6f354abeb2
fhernand <fhernand@users.noreply.github.com>
2018-11-29 23:07:38 +0100

    Fix for brightness

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ for (var i = 0; i < NUM_LEDS; i++) {
 			blackpixelData[i] = rgb2Int(0,0,0);
 }
 ws281x.init(NUM_LEDS);
-ws281x.setBrightness(0.5);
+ws281x.setBrightness(128);
 
 // ---- trap the SIGINT and reset before exit
 process.on('SIGINT', function () {
